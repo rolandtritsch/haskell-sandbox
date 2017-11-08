6de015db16f0ed629cd1d86215a16443f544b52a
Roland Tritsch <roland@gonito.com>
2017-11-08 17:02:12 +0000

    Tutorial: 09/10: Inital commit

---
+++ tutorial/09-ErrorHandling/Spoon.hs
@@ -0,0 +1,18 @@
+-- Spoon.hs
+module Spoon where
+
+import Control.Spoon
+
+goBoom :: Int -> Int -> Int
+goBoom x y = x `div` y
+
+test1 :: Maybe [Int]
+test1 = spoon [1, 2, undefined]
+
+test2 :: Maybe [Int]
+test2 = teaspoon [1, 2, undefined]
+
+main :: IO ()
+main = do
+  maybe (putStrLn "Nothing") (print . length) test1
+  maybe (putStrLn "Nothing") (print . length) test2
